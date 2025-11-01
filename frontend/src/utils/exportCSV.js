276d8b941ea4c0db976e320016d201e68e7b4213
alexreviaka <reviakaaliakseii@gmail.com>
2025-11-01 17:56:55 +0000

    feat: add export to CSV functionality

---
+++ frontend/src/utils/exportCSV.js
@@ -0,0 +1,9 @@
+export function exportToCSV(data, filename) {
+  const csv = data.map(row => Object.values(row).join(',')).join('\n');
+  const blob = new Blob([csv], { type: 'text/csv' });
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  a.click();
+}
